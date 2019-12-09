99822ab0487788bbfbf534d8d10c8191ec3577e8
SKJHA <SKJHA@DESKTOP-DQ50J0T>
2019-12-09 18:36:25 +0530

    usermgmt modal added

--- src/main/java/com/mystartup/conceptjava/modal/usermgmt/UserManagement.java
+++ src/main/java/com/mystartup/conceptjava/modal/usermgmt/UserManagement.java
@@ -1,6 +1,66 @@
 package com.mystartup.conceptjava.modal.usermgmt;
 
-public class UserManagement {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usermaster")
+public class UserManagement implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private String fullName;
+	private String mobileNo;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	
 
 }
