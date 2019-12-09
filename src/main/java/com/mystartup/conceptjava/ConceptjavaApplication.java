71947bbec05c32611f895a248fcbdae1870e980c
SKJHA <SKJHA@DESKTOP-DQ50J0T>
2019-12-09 16:45:45 +0530

    initial commit

---
+++ src/main/java/com/mystartup/conceptjava/ConceptjavaApplication.java
@@ -0,0 +1,13 @@
+package com.mystartup.conceptjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConceptjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConceptjavaApplication.class, args);
+	}
+
+}
