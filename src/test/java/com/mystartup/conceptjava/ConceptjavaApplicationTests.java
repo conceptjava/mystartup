31676071e36de53e1a76babe5fadfe29645049b9
SKJHA <SKJHA@DESKTOP-DQ50J0T>
2019-12-09 16:54:40 +0530

    Initial commit

---
+++ src/test/java/com/mystartup/conceptjava/ConceptjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystartup.conceptjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConceptjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
